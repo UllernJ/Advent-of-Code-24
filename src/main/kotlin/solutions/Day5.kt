460455bd3d632bdf442fa72a8a3831b9ddfc04bf
UllernJ <Ulrik.johannesen@gmail.com>
2024-12-07 20:05:13 +0100

    day5 a

---
+++ src/main/kotlin/solutions/Day5.kt
@@ -0,0 +1,52 @@
+package solutions
+
+import utils.readInput
+
+private val input = readInput("day5").map { it.split(",") }
+private val puzzle = input.filter { it.size > 2 }.map { it.map(String::toInt) }
+private val map = mutableMapOf<Int, MutableList<Int>>()
+private fun part1(): Int {
+    return calculateQueue()
+}
+
+private fun calculateQueue(): Int {
+    populateRuleset()
+    val validList = puzzle.filter { isValid(it) }
+    return validList.sumOf {
+        it[it.size/2]
+    }
+}
+
+private fun isValid(queue: List<Int>): Boolean {
+    val ruleset = extractRules(queue)
+    return ruleset != null
+            && ruleset.containsAll(queue.drop(1))
+            && containsIllegalNumber(queue)
+}
+
+private fun extractRules(list: List<Int>): MutableList<Int>? {
+    return map[list[0]]
+}
+
+private fun containsIllegalNumber(list: List<Int>): Boolean {
+    return list.drop(1)
+        .zipWithNext()
+        .all { (val1, val2) -> map[val1]?.contains(val2) == true }
+}
+
+private fun populateRuleset() {
+    val list = input.filter { it.size == 1 }.flatten()
+    list.forEach {
+        it.split("|").map(String::toInt).zipWithNext { val1, val2 ->
+            if (map.containsKey(val1)) {
+                map[val1]?.add(val2)
+            } else {
+                map[val1] = mutableListOf(val2)
+            }
+        }
+    }
+}
+
+fun main() {
+    println(part1())
+}
