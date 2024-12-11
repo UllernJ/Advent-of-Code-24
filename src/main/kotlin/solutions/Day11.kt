8c128dd5c68b4e2796f4f4665933e35098c29ae0
UllernJ <Ulrik.johannesen@gmail.com>
2024-12-11 20:42:34 +0100

    day11 part 1

---
+++ src/main/kotlin/solutions/Day11.kt
@@ -0,0 +1,41 @@
+package solutions
+
+import utils.readInput
+
+private val input = readInput("day11").flatMap {it.split(" ")}.map(String::toLong)
+
+private fun part1() {
+    val list = manipulateSpaceTime(input, 75)
+    println("Number of stones: ${list.size}")
+}
+
+private fun manipulateSpaceTime(input: List<Long>, iterations: Int): List<Long> {
+    if (iterations == 0) {
+        return input
+    }
+    val newList = mutableListOf<Long>()
+    input.forEach { value ->
+        val valueStr = value.toString()
+        if (value == 0.toLong()) {
+            newList.add(1)
+        } else if (valueStr.length % 2 == 0) {
+            val mid = valueStr.length / 2
+            val leftSide = valueStr.substring(0, mid)
+            val rightSide = valueStr.substring(mid)
+            newList.add(leftSide.toLong())
+            newList.add(rightSide.toLong())
+        } else {
+            newList.add(value * 2024)
+        }
+    }
+    return manipulateSpaceTime(newList, iterations - 1)
+}
+private fun String.trimTrailingZeros(): String {
+    return this.trimEnd('0').ifEmpty { "0" }
+}
+
+
+
+fun main() {
+    part1()
+}
